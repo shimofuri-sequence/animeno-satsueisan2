03ff3aa7f8a288cc551b6dbc6f4b4c59d3bc14ed
Yoshioka Hiroo <yoshioka.hiroo@gmail.com>
2019-08-11 23:35:30 +0900

    first commit

---
+++ getFiles.jsx
@@ -0,0 +1,21 @@
+﻿(function(){
+    
+    function tree(folder, depth){
+        var files = folder.getFiles();
+        var num = files.length;
+
+        for(var i=0;i<num;i++){
+            var file = files[i];
+            $.writeln(depth + File.decode(file.name));
+            if(file instanceof Folder){
+                var newDepth = '　';
+                tree(file, depth + newDepth);
+                }
+            }
+        }
+
+    var root = Folder.selectDialog ();
+    if(root!==null){
+        tree(root, '');
+        }
+})();
---
+++ getShapeProperties.jsx
@@ -0,0 +1,20 @@
+﻿(function(){
+    
+    function shape(s, depth){
+        for(var i=1;i<=s.numProperties;i++){
+            var prop = s.property(i);
+            $.writeln(depth, prop.name);
+            
+            if(prop instanceof PropertyGroup){
+                var newDepth = '　';
+                shape(prop, depth + newDepth);
+                }
+            }
+        }
+
+    var root = app.project.activeItem.selectedLayers[0];
+    var shapeProperty = root.property('ADBE Root Vectors Group');
+
+    shape(shapeProperty, '')
+    
+})();
